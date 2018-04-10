4d11b9588cfcd450d1f689a0bdd5b0861fc4fc71
Jason Swetzoff <swetzoff@gmail.com>
2018-04-09 22:17:16 -0700

    Add js.

--- src/js/main.js
+++ src/js/main.js
@@ -1,2 +1,14 @@
+// $(document).ready(function () {
+//   $(window).scroll(function () {
+//       if ($(document).scrollTop() > 100) {
+//           $(".header").addClass("scrolled");
+//       } else {
+//           $(".header").removeClass("scrolled");
+//       }
+//   });
+// });
+
 window.sr = ScrollReveal();
-sr.reveal('.foo');  
+sr.reveal('.project', { duration: 500, delay: 500 }, 50);
+sr.reveal('.about', { duration: 500, delay: 500 });
+
