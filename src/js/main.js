98293580d575ed4fc64982d2b41b03da99dac7b7
Jason  Swetzoff <jaswetz@gmail.com>
2025-03-18 15:31:48 -0700

    Update project titles, enhance styles, and add testimonials to index page

--- src/js/main.js
+++ src/js/main.js
@@ -10,5 +10,5 @@ $(document).ready(function () {
 
 window.sr = ScrollReveal();
 sr.reveal('.project', { duration: 500, delay: 500 }, 50);
-sr.reveal('.about', { duration: 500, delay: 500 });
-
+sr.reveal('.about', { duration: 500, delay: 500 }, 50);
+sr.reveal('.blurb', { duration: 500, delay: 500 }, 50);
